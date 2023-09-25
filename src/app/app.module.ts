7f7185f3512283730cd40954bec4e437d73a9968
Spolito24 <baptisterenier69@gmail.com>
2023-09-25 21:04:16 +0200

    set the basic need for the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokedexPageComponent } from './pages/pokedex-page/pokedex-page.component';
+import { CreatePokemonComponent } from './pages/create-pokemon/create-pokemon.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokedexPageComponent,
+    CreatePokemonComponent,
+    MenuComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
