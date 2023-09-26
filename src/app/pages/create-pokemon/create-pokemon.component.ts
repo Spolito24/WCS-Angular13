319e02c17db5458071f794b517bbea7690c22302
Spolito24 <baptisterenier69@gmail.com>
2023-09-26 15:26:55 +0200

    finish the pokemon form

--- src/app/pages/create-pokemon/create-pokemon.component.ts
+++ src/app/pages/create-pokemon/create-pokemon.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Pokemon } from 'src/app/shared/Pokemon';
 
 @Component({
   selector: 'app-create-pokemon',
   templateUrl: './create-pokemon.component.html',
-  styleUrls: ['./create-pokemon.component.scss']
+  styleUrls: ['./create-pokemon.component.scss'],
 })
 export class CreatePokemonComponent {
+  newPokemon: Pokemon = new Pokemon('', '', '');
 
+  constructor() {}
+
+  onSubmit(): void {
+    console.log(this.newPokemon);
+  }
 }
